5282c9fb5eafb481d67ff507e58ba56cd7a68f55
Vinícius Andrade <135062180+ViniciusAndradeCosta@users.noreply.github.com>
2024-10-01 01:53:10 -0300

    Update and rename questions to questions.js

---
+++ questions.js
@@ -0,0 +1,146 @@
+let questions = [
+    {
+        numb: 1,
+        question: "- Qual das seguintes afirmações melhor descreve o impacto do ESG na redução de custos para as empresas?",
+        answer: "D. Empresas que implementam processos produtivos mais sustentáveis, como uso racional de recursos e redução de desperdícios, podem ver uma significativa diminuição de custos operacionais.",
+        options: [
+            "A. ESG aumenta os custos operacionais devido à necessidade de implementar novas tecnologias sustentáveis.",
+            "B. A adoção de práticas ESG não tem relação direta com a redução de custos nas empresas.",
+            "C. ESG reduz custos principalmente através da diminuição de multas e sanções regulatórias.",
+            "D. Empresas que implementam processos produtivos mais sustentáveis, como uso racional de recursos e redução de desperdícios, podem ver uma significativa diminuição de custos operacionais."
+            
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "- Como as práticas ESG impactam a produtividade de uma empresa?",
+        answer: "C. Políticas que incentivam a diversidade, inclusão e bem-estar dos funcionários criam ambientes de trabalho mais positivos e colaborativos, elevando a produtividade.",
+        options: [
+            "A. ESG não tem impacto significativo na produtividade dos funcionários.",
+            "B. A implementação de práticas ESG geralmente resulta em uma diminuição da produtividade devido ao foco em questões não relacionadas ao negócio principal.",
+            "C. Políticas que incentivam a diversidade, inclusão e bem-estar dos funcionários criam ambientes de trabalho mais positivos e colaborativos, elevando a produtividade.",
+            "D. ESG aumenta a produtividade exclusivamente através da automação de processos para reduzir o impacto ambiental."
+            
+        ]
+    },
+
+
+    {
+        numb: 3,
+        question: "- Qual é o principal benefício da adoção de critérios ESG em relação às intervenções regulatórias?",
+        answer: "D. Empresas com padrões ESG elevados têm uma relação mais harmoniosa com órgãos reguladores, reduzindo riscos de sanções e ganhando mais liberdade operacional.",
+        options: [
+            "A. ESG garante que as empresas nunca serão alvo de regulamentações governamentais.",
+            "B. A adoção de ESG não tem relação com intervenções regulatórias.",
+            "C. ESG permite que as empresas ignorem completamente as regulamentações existentes.",
+            "D. Empresas com padrões ESG elevados têm uma relação mais harmoniosa com órgãos reguladores, reduzindo riscos de sanções e ganhando mais liberdade operacional."
+            
+        ]
+    },
+
+
+
+    {
+        numb: 4,
+        question: "- Como o ESG afeta a otimização de ativos e investimentos de uma empresa?",
+        answer: "B. Direcionar capital para iniciativas sustentáveis melhora a eficiência operacional e o retorno sobre os investimentos, preparando as empresas para se adaptar a novos padrões de mercado.",
+        options: [
+            "A. ESG não tem impacto significativo na forma como as empresas alocam seus recursos.",
+            "B. Direcionar capital para iniciativas sustentáveis melhora a eficiência operacional e o retorno sobre os investimentos, preparando as empresas para se adaptar a novos padrões de mercado.",
+            "C. A adoção de ESG geralmente resulta em uma diminuição do retorno sobre investimentos devido aos altos custos iniciais.",
+            "D. ESG foca exclusivamente em investimentos ambientais, negligenciando outras áreas de otimização de ativos."
+            
+        ]
+    },
+
+
+    {
+        numb: 5,
+        question: "- Qual é a principal consequência para empresas que falham em implementar critérios ESG?",
+        answer: "B. Empresas com fraco desempenho em ESG estão mais expostas a riscos, podendo enfrentar maior volatilidade no valor de suas ações e custos de financiamento mais altos.",
+        options: [
+            "A. Não há consequências significativas para empresas que não adotam práticas ESG.",
+            "B. Empresas com fraco desempenho em ESG estão mais expostas a riscos, podendo enfrentar maior volatilidade no valor de suas ações e custos de financiamento mais altos.",
+            "C. A única consequência é uma leve diminuição na satisfação dos funcionários.",
+            "D.  Empresas que não adotam ESG são automaticamente excluídas de todos os mercados internacionais."
+            
+        ]
+    },
+
+    {
+    numb: 6,
+        question: "- Qual é a principal consequência para empresas que falham em implementar critérios ESG?",
+        answer: "D. Empresas que reduzem emissões e otimizam recursos tendem a se posicionar de forma mais competitiva.",
+        options: [
+            "A. Não tem impacto significativo na competitividade.",
+            "B. Aumenta os custos operacionais, reduzindo a competitividade.",
+            "C. Afeta apenas a imagem da empresa, sem impacto econômico real.",
+            "D. Empresas que reduzem emissões e otimizam recursos tendem a se posicionar de forma mais competitiva."
+
+
+        ]
+    },
+
+
+    {
+        numb: 7,
+            question: "- Qual é o potencial econômico da transição para uma economia verde no Brasil, de acordo com o texto?",
+            answer: "B. Pode injetar R$ 2,8 trilhões na economia até 2030.",
+            options: [
+                "A. Não há potencial econômico significativo.",
+                "B. Pode injetar R$ 2,8 trilhões na economia até 2030.",
+                "C. Gera apenas custos adicionais para as empresas.",
+                "D. O texto não menciona nenhum potencial econômico específico."
+    
+            ]
+        },
+
+    
+    {
+    numb: 8,
+        question: "- Como as práticas ESG afetam a relação das empresas com os órgãos reguladores?",
+        answer: "D. Empresas com padrões ESG elevados têm uma relação mais harmoniosa com órgãos reguladores.",
+        options: [
+            "A. Não têm impacto na relação com reguladores.",
+            "B. Aumentam o risco de intervenções regulatórias.",
+            "C. Dificultam o diálogo com as autoridades.",
+            "D. Empresas com padrões ESG elevados têm uma relação mais harmoniosa com órgãos reguladores."
+
+
+        ]
+    },
+
+
+    {
+        numb: 9,
+            question: "- Qual é o impacto das práticas sociais do ESG na produtividade de uma empresa?",
+            answer: "C. Aumentam a produtividade ao criar ambientes de trabalho mais positivos e colaborativos.",
+            options: [
+                "A. Reduzem a produtividade devido ao foco em questões não relacionadas ao negócio.",
+                "B. Não têm impacto significativo na produtividade.",
+                "C. Aumentam a produtividade ao criar ambientes de trabalho mais positivos e colaborativos.",
+                "D. Afetam apenas a imagem da empresa, sem impacto na produtividade."
+    
+    
+            ]
+    },
+
+
+
+    {
+        numb: 10,
+            question: "- Qual é a principal consequência para empresas que não adotam práticas ESG, segundo o texto?",
+            answer: "B. Enfrentam maior volatilidade no valor das ações e custos de financiamento mais altos.",
+            options: [
+                "A. Não há consequências significativas.",
+                "B. Enfrentam maior volatilidade no valor das ações e custos de financiamento mais altos.",
+                "C. São excluídas automaticamente do mercado.",
+                "D. Perdem apenas uma pequena parcela de clientes conscientes."
+                    
+    
+            ]
+    },
+
+
+];
